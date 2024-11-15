f3eece935323d8401145bb6a0bec1f681ece7612
12960_ux (Student) <delcroixphilippe73@gmail.com>
2024-11-15 16:24:05 +0100

    peut etre probleme de prod reglé lol

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: '_redirects',
-          dest: '.'
-        }
-      ]
-    })
-  ],
-  base: mode === 'production' ? '/quynh-yen-kohnert-react/' : '/', // Base conditionnelle
+  plugins: [react()],
+  base: mode === 'production' ? '/quynh-yen-kohnert-react/' : '/', // Gestion conditionnelle de base
 }));
