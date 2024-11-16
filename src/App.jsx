9597fb374ceccd1a84caf2429e87de9f1b0f0f08
philouLeF <delcroixphilippe73@gmail.com>
2024-11-16 18:15:41 +0100

    deploy netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import CreationsPage from './pages/CreationsPage';
-import ArtworkPage from './pages/ArtworkPage';
-import HomePage from './pages/HomePage';
-import Contact from './pages/Contact';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import CreationsPage from "./pages/CreationsPage";
+import ArtworkPage from "./pages/ArtworkPage";
+import HomePage from "./pages/HomePage";
+import Contact from "./pages/Contact";
 
 const App = () => {
   return (
-    <BrowserRouter basename={process.env.NODE_ENV === 'production' ? '/quynh-yen-kohnert-react' : '/'}>
+    <Router>
       <Layout>
         <Routes>
           <Route path="/" element={<HomePage />} />
@@ -17,7 +17,7 @@ const App = () => {
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </Layout>
-    </BrowserRouter>
+    </Router>
   );
 };
 
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: mode === 'production' ? '/quynh-yen-kohnert-react/' : '/', // Gestion conditionnelle de base
-}));
+  base: "/", // Aucun chemin spécifique pour Netlify
+});
